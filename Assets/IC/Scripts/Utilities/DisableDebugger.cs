790d52a74be96e2d6d282b139dfea2aa061414db
Pedro de Paula <pedrodepaula2005@gmail.com>
2026-08-25 14:14:16 -0300

    [ADD] Microgestures package

--- Assets/IC/Scripts/Utilities/DisableDebugger.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DisableDebugger : MonoBehaviour
-{
-    void OnDisable()
-    {
-        Debug.Log($"{name} foi desativado!\n{System.Environment.StackTrace}");
-    }
-}
