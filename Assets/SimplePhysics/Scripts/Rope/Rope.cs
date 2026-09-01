2792d33fef1533ad7a6be00d37759d5e35599bf8
edupinho69 <eduardo.pinho.2006@gmail.com>
2026-09-01 15:24:03 -0300

    Ajuste Corda VR
    XR Toolkit

--- Assets/SimplePhysics/Scripts/Rope/Rope.cs
+++ Assets/SimplePhysics/Scripts/Rope/Rope.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,20 +6,23 @@ public class Rope : MonoBehaviour
 {
     [Header("RigidBodies")]
     public List<Transform> points = new List<Transform>();
+    [SerializeField] public float distanciaCordas;
 
     [Header("Appearance")]
     public Material lineMaterial;
     public bool showLineRenderers = true;
 
     [Header("Bones & Rope")]
-    public Transform Bone0;
+    public Rigidbody BoneGarra;
     private LineRenderer lr;
+    private int primeira = 0;
 
     public void Init(List<Transform> pts, Material lineMaterial)
     {
         points = pts;
         this.lineMaterial = lineMaterial;
     }
+
     private void Awake()
     {
         if (!showLineRenderers)
@@ -32,6 +36,7 @@ public class Rope : MonoBehaviour
         lr.endWidth = 0.189f;
         lr.generateLightingData = true;
     }
+
     void LateUpdate()
     {
         if (!showLineRenderers || lr == null || points == null || points.Count == 0)
@@ -42,17 +47,39 @@ public class Rope : MonoBehaviour
 
     void UpdateLineRenderer()
     {
-        for (int i = 0; i < points.Count; i++)
-        {
-            if (i == 0) lr.SetPosition(0, points[1].position);
-            else lr.SetPosition(i, points[i].position);
-        }
+        //if (verifyBonesDistance() || 0 == primeira++)
+        //{
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (i == 0) lr.SetPosition(0, points[1].position);
+                else lr.SetPosition(i, points[i].position);
+            }
+        //}
     }
 
     private void Update()
     {
-        Bone0.transform.localPosition = new Vector3(0f, 0f, 0f);
+        points[0].localPosition = new Vector3(0f, 0f, 0f);
     }
 
+    bool verifyBonesDistance()
+    {
+        for (int i = 0; i < points.Count-1; i++)
+        {
+            var valor = (float)Math.Round((double)Vector3.Distance(points[i].position, points[i+1].position), 2);
+            if (valor <= 1.2f)
+            {
+                Debug.Log("Menor: " + valor);
+            }
+            else
+            {
+                if (BoneGarra.isKinematic == false) return true;
+                lr.SetPosition(18, points[18].position);
+                Debug.Log("Maior: "+ i + " " + valor);
+                return false;
+            }
+            }
+            return true;
+    }
 }
 
